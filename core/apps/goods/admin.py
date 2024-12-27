d45c7b4277ec71d911665ba4b27fafd5265ca3f9
MaxBakshaev <risen.2011@mail.ru>
2024-12-27 12:24:24 +0900

    Добавлены методы для отображения информации о товарах, комментарии к параметрам в админ панели

--- core/apps/goods/admin.py
+++ core/apps/goods/admin.py
@@ -6,19 +6,34 @@ from goods.models import Categories, Products
 # Регистрация моделей в админ панели
 
 @admin.register(Categories)
-class CategoriesAdmin(admin.ModelAdmin):
+class CategoriesAdmin(admin.ModelAdmin):  
+    
+    # отображение в http://127.0.0.1:8000/admin/goods/categories/ :
+
     # автоматически набирается текст для ссылки после ввода названия
-    prepopulated_fields = {"slug": ("name",)}
+    prepopulated_fields = {"slug": ("name",)}  
+    # отображение полей на странице списка категорий
     list_display = ["name",]
     
-    
+
 @admin.register(Products)
-class ProductsAdmin(admin.ModelAdmin):
+class ProductsAdmin(admin.ModelAdmin):  
+    
+    # отображение в http://127.0.0.1:8000/admin/goods/products/ :
+     
+    # автоматически набирается текст для ссылки после ввода названия
     prepopulated_fields = {"slug": ("name",)}
+    # отображение полей на странице списка продуктов
     list_display = ["name", "quantity", "price", "discount"]
-    list_editable = ["discount",]
+    # редактируемые параметры
+    list_editable = ["discount",]  
+    # поиск по параметрам
     search_fields = ["name", "description"]
+    # фильтр по скидке, количеству, категории
     list_filter = ["discount", "quantity", "category"]
+    
+    # Отображение на странице добавления или изменения параметров товара :
+    
     # в документации много разных полей в разделе admin settings
     fields = [
         "name",
--- core/apps/goods/models.py
+++ core/apps/goods/models.py
@@ -23,7 +23,7 @@ class Products(models.Model):
     image = models.ImageField(upload_to='goods_images', blank=True, null=True, verbose_name='Изображение')
     # max_digits и decimal_places - количество знаков до и после запятой
     price = models.DecimalField(default=0.00, max_digits=20, decimal_places=0, verbose_name='Цена')
-    discount = models.DecimalField(default=0.00, max_digits=20, decimal_places=2, verbose_name='Скидка в %')
+    discount = models.DecimalField(default=0.00, max_digits=20, decimal_places=0, verbose_name='Скидка в %')
     # PositiveIntegerField не может содержать отрицательное чисто в отличие от IntegerField
     quantity = models.PositiveIntegerField(default=0, verbose_name='Количество')
     # ForeignKey связывает записи из таблицы Product с таблицей Categories
@@ -36,5 +36,17 @@ class Products(models.Model):
         verbose_name = 'Продукт'
         verbose_name_plural = 'Продукты'
 
+    # метод для отображения количества в админке
     def __str__(self):
         return f'{self.name} Количество - {self.quantity}'
+    
+    # метод для отображения id с количеством знаков
+    def display_id(self):
+        return f'{self.id:05}'
+    
+    # метод для отображения стоимости товара с учетом скидки
+    def sell_price(self):
+        if self.discount:
+            return round(self.price - self.price*self.discount/100, 0)
+        
+        return self.price
