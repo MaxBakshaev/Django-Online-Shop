6d968d014a0c769dc8d1b1dc7f585fd9d0d2611c
MaxBakshaev <risen.2011@mail.ru>
2025-01-07 16:46:47 +0900

    Подключен postgresql и добавлен полноценный поиск

--- core/apps/goods/utils.py
+++ core/apps/goods/utils.py
@@ -1,22 +1,30 @@
+from django.contrib.postgres.search import SearchQuery, SearchRank, SearchVector
 from django.db.models import Q
 
 from goods.models import Products
 
 
 def q_search(query):
+    """Поиск по id, словам в названии и описании"""
     
     # проверка по длине символов в поиске
     if query.isdigit() and len(query) <= 5:
         return Products.objects.filter(id=int(query))
     
-    # список слов в поиске для дальнейшей обработки
-    keywords = [word for word in query.split() if len(word) > 2]
+    # упорядочение по релевантности
+    vector = SearchVector("name", "description")
+    query = SearchQuery(query)
     
-    q_objects = Q()
+    return Products.objects.annotate(rank=SearchRank(vector, query)).order_by("-rank")
     
-    # все слова объединяются в один запрос
-    for token in keywords:
-        q_objects |= Q(description__icontains=token)
-        q_objects |= Q(name__icontains=token)
+    # # список слов в поиске для дальнейшей обработки
+    # keywords = [word for word in query.split() if len(word) > 2]
+    
+    # q_objects = Q()
+    
+    # # все слова объединяются в один запрос
+    # for token in keywords:
+    #     q_objects |= Q(description__icontains=token)
+    #     q_objects |= Q(name__icontains=token)
         
-    return Products.objects.filter(q_objects)
+    # return Products.objects.filter(q_objects)
