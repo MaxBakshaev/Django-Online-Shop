a29a72440589436021dd615535e3f09c492afffb
MaxBakshaev <risen.2011@mail.ru>
2025-01-16 20:29:38 +0900

    Добавлен тег для отображения товаров в корзине

--- core/apps/carts/models.py
+++ core/apps/carts/models.py
@@ -35,7 +35,7 @@ class Cart(models.Model):
         
     objects = CartQueryset().as_manager()    
         
-    def product_price(self):
+    def products_price(self):
         return round(self.product.sell_price() * self.quantity, 2)
       
     def __str__(self):
