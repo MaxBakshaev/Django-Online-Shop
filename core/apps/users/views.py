6a8242b6c195aca437f94e6f2ff5b2ccd00a1ff9
MaxBakshaev <risen.2011@mail.ru>
2025-01-22 13:50:11 +0900

    Добавлен сессионный ключ для анонимного пользователя

--- core/apps/users/views.py
+++ core/apps/users/views.py
@@ -4,6 +4,7 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import redirect, render
 from django.urls import reverse
 
+from carts.models import Cart
 from users.forms import ProfileForm, UserLoginForm, UserRegistrationForm
 
 
@@ -16,10 +17,15 @@ def login(request):
             password = request.POST['password']
             user = auth.authenticate(username=username, password=password)
         
+            session_key = request.session.session_key
+            
             if user:
                 auth.login(request, user)
                 messages.success(request, f"{username}, Вы вошли в аккаунт")
                 
+                if session_key:
+                    Cart.objects.filter(session_key=session_key).update(user=user)
+                    
                 redirect_page = request.POST.get('next', None)
                 if redirect_page and redirect_page != reverse('user:logout'):
                     return HttpResponseRedirect(request.POST.get('next'))
@@ -41,10 +47,17 @@ def registration(request):
     if request.method == 'POST':
         form = UserRegistrationForm(data=request.POST)
         if form.is_valid():
-            form.save()          
+            form.save()
+            
+            session_key = request.session.session_key
+                      
             user = form.instance
             # после успешной регистрации пользователь автоматически авторизируется
             auth.login(request, user)
+            
+            if session_key:
+                Cart.objects.filter(session_key=session_key).update(user=user)
+                
             messages.success(request, f"{user.username}, Вы успешно зарегистрированы и вошли в аккаунт")
             
             return HttpResponseRedirect(reverse('main:index'))
