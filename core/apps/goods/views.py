d19446917327340bd70f9216fc7f7a8a8e9fecea
MaxBakshaev <risen.2011@mail.ru>
2025-03-02 03:30:15 +0900

    Добавлены количество отзывов на главной странице, изменен способ получения оценок на главной странице

--- core/apps/goods/views.py
+++ core/apps/goods/views.py
@@ -166,6 +166,149 @@ def product(request, product_slug) -> HttpResponseRedirect | HttpResponse:
     )
 
 
+# class CatalogView(ListView):
+
+#     model = Products
+#     # queryset = Products.objects.all()
+#     template_name = "goods/catalog.html"
+#     context_object_name = "goods"
+#     paginate_by = 6
+
+#     def get_queryset(self) -> QuerySet[Any]:
+
+#         self.category_slug = self.kwargs.get("category_slug")
+
+#         # параметры для фильтров
+#         on_sale: str | None = self.request.GET.get("on_sale")
+#         order_by: str | None = self.request.GET.get("order_by")
+#         self.query: str | None = self.request.GET.get("q")
+#         cost_1k: str | None = self.request.GET.get("cost_1k")
+#         cost_10k: str | None = self.request.GET.get("cost_10k")
+#         cost_100k: str | None = self.request.GET.get("cost_100k")
+#         cost_1m: str | None = self.request.GET.get("cost_1m")
+#         cost_10m: str | None = self.request.GET.get("cost_10m")
+
+#         # создается базовый запрос к БД (QuerySet)
+#         if self.category_slug == "vse-tovary":
+#             goods: QuerySet[Any] = super().get_queryset()
+
+#         elif self.query:
+#             goods = q_search(self.query)
+
+#         else:
+#             goods = super().get_queryset().filter(category__slug=self.category_slug)
+
+#         # Проверки с добавлением фильтров к запросу
+#         if on_sale:
+#             goods = goods.filter(discount__gt=0)
+
+#         if order_by and order_by != "default":
+#             goods = goods.order_by(order_by)
+
+#         # Eсли выбран 1 параметр цены
+#         if cost_1k and not cost_10k and not cost_100k and not cost_1m and not cost_10m:
+#             goods = goods.filter(price__lt=1_000)
+
+#         if cost_10k and not cost_1k and not cost_100k and not cost_1m and not cost_10m:
+#             goods = goods.filter(price__lt=10_000, price__gt=1_000)
+
+#         if cost_100k and not cost_1k and not cost_10k and not cost_1m and not cost_10m:
+#             goods = goods.filter(price__lt=100_000, price__gt=10_000)
+
+#         if cost_1m and not cost_1k and not cost_10k and not cost_100k and not cost_10m:
+#             goods = goods.filter(price__lt=1_000_000, price__gt=100_000)
+
+#         if cost_10m and not cost_1k and not cost_10k and not cost_100k and not cost_1m:
+#             goods = goods.filter(price__gt=1_000_000)
+
+#         # Eсли выбрано 2 параметра цены
+#         if cost_1k and cost_10k and not cost_100k and not cost_1m and not cost_10m:
+#             goods = goods.filter(price__lt=10_000)
+
+#         if cost_10k and cost_100k and not cost_1k and not cost_1m and not cost_10m:
+#             goods = goods.filter(price__lt=100_000, price__gt=10_000)
+
+#         if cost_100k and cost_1m and not cost_1k and not cost_10k and not cost_10m:
+#             goods = goods.filter(price__lt=1_000_000, price__gt=100_000)
+
+#         if cost_1m and cost_10m and not cost_1k and not cost_10k and not cost_100k:
+#             goods = goods.filter(price__gt=1_000_000)
+
+#         # Eсли выбрано 3 параметра цены
+#         if cost_1k and cost_10k and cost_100k and not cost_1m and not cost_10m:
+#             goods = goods.filter(price__lt=100_000)
+
+#         if cost_10k and cost_100k and cost_1m and not cost_1k and not cost_10m:
+#             goods = goods.filter(price__lt=1_000_000, price__gt=10_000)
+
+#         if cost_100k and cost_1m and cost_10m and not cost_1k and not cost_10k:
+#             goods = goods.filter(price__gt=100_000)
+
+#         # Eсли выбрано 4 параметра цены
+#         if cost_1k and cost_10k and cost_100k and cost_1m and not cost_10m:
+#             goods = goods.filter(price__lt=1_000_000)
+
+#         if cost_10k and cost_100k and cost_1m and cost_10m and not cost_1k:
+#             goods = goods.filter(price__gt=1_000)
+
+#         self.amount = len(goods)
+
+#         goods_rating_list = []
+#         goods_amount_reviews_list = []
+
+#         for product in goods:
+
+#             # получение количества отзывов о продуктах
+#             product_reviews = product.reviews.all()
+#             product_amount_reviews: int = len(product_reviews)
+#             goods_amount_reviews_list.append(product_amount_reviews)
+
+#             # получение оценки продуктов
+#             product_rate = 0
+#             for product_review in product_reviews:
+#                 product_rate += product_review.rating
+#             try:
+#                 product_rating = product_rate / product_amount_reviews
+#             except ZeroDivisionError:
+#                 product_rating = 0
+
+#             goods_rating_list.append(product_rating)
+
+#         self.dict_product_rating_amount_reviews = {}
+#         product_index = 0
+#         for product_element in goods:
+#             # в словарь передаются идентификатор продукта - ключ, оценка и количество отзывов - значение
+#             self.dict_product_rating_amount_reviews[goods[product_index]] = (
+#                 goods_rating_list[product_index],
+#                 goods_amount_reviews_list[product_index],
+#             )
+#             product_index += 1
+
+#         return goods
+
+#     def get_context_data(self, **kwargs) -> dict[str, Any]:
+
+#         # контекстные переменные при поиске
+#         context: dict[str, Any] = super().get_context_data(**kwargs)
+#         context["title"] = "MultiShop - Каталог - Поиск"
+#         context["check_page"] = "MultiShop - Категории"
+#         context["amount"] = self.amount
+
+#         # если не поиск, то добавляются переменные
+#         if not self.query:
+#             category = Categories.objects.get(slug=self.category_slug)
+
+#             context["dict_product_rating_amount_reviews"] = (
+#                 self.dict_product_rating_amount_reviews
+#             )
+#             context["title"] = category.name
+#             context["slug_url"] = self.category_slug
+#             context["category"] = category
+
+#         return context
+
+
+
 # class ProductView(DetailView):
 
 #     # model = Products
--- core/apps/main/views.py
+++ core/apps/main/views.py
@@ -2,73 +2,130 @@ from typing import Any
 from django.views.generic import TemplateView
 
 from common.mixins import CacheMixin
-from goods.models import Products, Review
+from goods.models import Products
 
 
 class IndexView(TemplateView, CacheMixin):
-    
-    template_name = 'main/index.html'
-    
-    def get_product_rating(self, new_product_rating):
-        return new_product_rating.pop(0)
+
+    template_name = "main/index.html"
 
     def get_context_data(self, **kwargs) -> dict[str, Any]:
-        
-        context: dict[str, Any] = super().get_context_data(**kwargs)           
-        context['title'] = 'MultiShop - Главная' 
-        # лучшие скидки
-        discount_products = ((Products.objects.all()).filter(discount__gt=0)).order_by('-discount')[:4]
-        context["discount_products"] = self.set_get_cache(discount_products, f"user_{self.request.user.id}_discount_products", 600)
-        
-        # новинки
-        new_products = Products.objects.all().order_by('-id')[:4]
-        context["new_products"] = self.set_get_cache(new_products, f"user_{self.request.user.id}_new_products", 600)
-        
-        new_product_rating = []
-        amount_reviews_list = []
-        
-        for new_product in new_products:
-            reviews = new_product.reviews.all()
-            amount_reviews: int = len(reviews)
-            amount_reviews_list.append(amount_reviews)
-            
-            rate = 0
-            for review in reviews:
-                rate += review.rating
+        """
+        discount_product - dp
+        new_product - np
+        """
+
+        context: dict[str, Any] = super().get_context_data(**kwargs)
+        context["title"] = "MultiShop - Главная"
+
+        # запрос по 4 продуктам с лучшими скидками
+        discount_products = ((Products.objects.all()).filter(discount__gt=0)).order_by(
+            "-discount"
+        )[:4]
+        context["discount_products"] = self.set_get_cache(
+            discount_products, f"user_{self.request.user.id}_discount_products", 600
+        )
+
+        dp_rating_list = []
+        dp_amount_reviews_list = []
+
+        for dp in discount_products:
+
+            # получение количества отзывов о продуктах по скидке
+            dp_reviews = dp.reviews.all()
+            dp_amount_reviews: int = len(dp_reviews)
+            dp_amount_reviews_list.append(dp_amount_reviews)
+
+            # получение оценки продуктов по скидке
+            dp_rate = 0
+            for dp_review in dp_reviews:
+                dp_rate += dp_review.rating
             try:
-                product_rating = rate/amount_reviews
+                dp_rating = dp_rate / dp_amount_reviews
             except ZeroDivisionError:
-                product_rating = 0
-                
-            new_product_rating.append(product_rating)
-        
-        dict_product_rating = dict(zip(new_products, new_product_rating))
-        context["dict_product_rating"] = dict_product_rating
+                dp_rating = 0
+
+            dp_rating_list.append(dp_rating)
+
+        dict_discount_product_rating_amount_reviews = {}
+        dp_index = 0
+        for dp_element in discount_products:
+            # в словарь передаются идентификатор продукта - ключ, оценка и количество отзывов - значение
+            dict_discount_product_rating_amount_reviews[discount_products[dp_index]] = (
+                dp_rating_list[dp_index],
+                dp_amount_reviews_list[dp_index],
+            )
+            dp_index += 1
+
+        context["dict_discount_product_rating_amount_reviews"] = (
+            dict_discount_product_rating_amount_reviews
+        )
+
+        # запрос по 4 новым продуктам
+        new_products = Products.objects.all().order_by("-id")[:4]
+        context["new_products"] = self.set_get_cache(
+            new_products, f"user_{self.request.user.id}_new_products", 600
+        )
+
+        np_rating_list = []
+        np_amount_reviews_list = []
+
+        for np in new_products:
+
+            # получение количества отзывов о новых продуктах
+            np_reviews = np.reviews.all()
+            np_amount_reviews: int = len(np_reviews)
+            np_amount_reviews_list.append(np_amount_reviews)
+
+            # получение оценки новых продуктов
+            np_rate = 0
+            for np_review in np_reviews:
+                np_rate += np_review.rating
+            try:
+                np_rating = np_rate / np_amount_reviews
+            except ZeroDivisionError:
+                np_rating = 0
+
+            np_rating_list.append(np_rating)
+
+        dict_new_product_rating_amount_reviews = {}
+        np_index = 0
+        for np_element in new_products:
+            # в словарь передаются идентификатор продукта - ключ, оценка и количество отзывов - значение
+            dict_new_product_rating_amount_reviews[new_products[np_index]] = (
+                np_rating_list[np_index],
+                np_amount_reviews_list[np_index],
+            )
+            np_index += 1
+
+        context["dict_new_product_rating_amount_reviews"] = (
+            dict_new_product_rating_amount_reviews
+        )
 
         return context
 
 
 class AboutView(TemplateView):
-    
-    template_name = 'main/about.html'
+
+    template_name = "main/about.html"
 
     def get_context_data(self, **kwargs) -> dict[str, Any]:
-        
-        context: dict[str, Any] = super().get_context_data(**kwargs)           
-        context['title'] = 'MultiShop - О нас'
-        context['content'] = 'MultiShop - Товары на все случаи жизни'
-        context['text_on_page'] = 'Лучший магазин во вселенной'
-        
+
+        context: dict[str, Any] = super().get_context_data(**kwargs)
+        context["title"] = "MultiShop - О нас"
+        context["content"] = "MultiShop - Товары на все случаи жизни"
+        context["text_on_page"] = "Лучший магазин во вселенной"
+
         return context
 
 
 class All_categories(TemplateView):
-    
-    template_name = 'main/all_categories.html'
+
+    template_name = "main/all_categories.html"
 
     def get_context_data(self, **kwargs) -> dict[str, Any]:
-        
-        context: dict[str, Any] = super().get_context_data(**kwargs)           
-        context['title'] = 'MultiShop - Каталог'
-        
+
+        context: dict[str, Any] = super().get_context_data(**kwargs)
+        context["title"] = "MultiShop - Каталог"
+
         return context
