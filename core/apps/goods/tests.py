3f94d5ab0b0a552f179dc58069994ef99c325721
MaxBakshaev <risen.2011@mail.ru>
2025-03-26 16:46:06 +0900

    Тесты для продуктов и заказов

--- core/apps/goods/tests.py
+++ core/apps/goods/tests.py
@@ -1,26 +1,207 @@
 from django.test import TestCase
 
-from goods.models import Categories, Products
+from users.models import User
+from goods.utils import q_search
+from goods.models import Categories, Products, Review
 
 
 class TestGoods(TestCase):
-    
-    def setUp(self):
-        Categories.objects.create(name='Книги', slug='knigi', id=1)
-        Categories.objects.create(name='Автомобили', slug='avtomobili', id=2)
-        Products.objects.create(name='Лимузин', slug='limuzin', price=100, discount=10, quantity=10, category_id=2)
 
-    def test_search(self):
-        response = self.client.get('/catalog/search/?q=стол/')
-        self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'goods/catalog.html')
+    def setUp(self):
+        self.test_user1 = User.objects.create_user(username='testuser1', password='12345')
+        self.test_user1.save()
+        Categories.objects.create(name="Книги", slug="knigi", id=1)
+        Categories.objects.create(name="Test Category", slug="test-category", id=3)
+        Categories.objects.create(name="Автомобили", slug="avtomobili", id=2)
+        self.product1 = Products.objects.create(
+            name="Лимузин",
+            slug="limuzin",
+            price=100,
+            discount=10,
+            quantity=1,
+            category_id=2,
+            id=1,
+        )
+        self.product2 = Products.objects.create(
+            name="Tesla Cybertruck",
+            slug="tesla-cybertruck",
+            price=100000,
+            discount=0,
+            quantity=2,
+            category_id=2,
+            id=2,
+        )
+        self.review = Review.objects.create(
+            product=self.product1,
+            user=self.test_user1,
+            rating=4,
+            comment='Хороший продукт',
+        )
         
+    def test_search_by_name(self):
+        response = self.client.get("/catalog/search/?q=Лимузин/", {"q": "Лимузин"})
+        self.assertEqual(len(response.context["goods"]), 1)
+        self.assertEqual(response.context["goods"][0][0], self.product1)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "goods/catalog.html")
+
+    def test_search_by_id(self):
+        response = self.client.get("/catalog/search/?q=2/", {"q": "2"})
+        self.assertEqual(len(response.context["goods"]), 1)
+        self.assertEqual(response.context["goods"][0][0], self.product2)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "goods/catalog.html")
+
+    def test_search_no_results(self):
+        response = self.client.get("/catalog/search/?q=product/", {"q": "product"})
+        self.assertEqual(len(response.context["goods"]), 0)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "goods/catalog.html")
+
+    def test_q_search_by_id(self):
+        result = q_search("1")
+        self.assertEqual(result.count(), 1)
+        self.assertEqual(result.first(), self.product1)
+
+    def test_q_search_by_name(self):
+        result = q_search("Лимузин")
+        self.assertEqual(result.count(), 1)
+        self.assertEqual(result.first(), self.product1)
+
+    def test_q_search_no_results(self):
+        result = q_search("xbbgsffgbgbf")
+        self.assertEqual(result.count(), 0)
+
     def test_category(self):
-        response = self.client.get('/catalog/knigi/')
+        response = self.client.get("/catalog/knigi/")
         self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'goods/catalog.html')
-        
+        self.assertTemplateUsed(response, "goods/catalog.html")
+
     def test_product(self):
-        response = self.client.get('/catalog/product/limuzin/')
+        response = self.client.get("/catalog/product/limuzin/")
         self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'goods/product.html')
+        self.assertTemplateUsed(response, "goods/product.html")
+
+    def test_get_queryset_all_products(self):
+        response = self.client.get(
+            "/catalog/avtomobili/", kwargs={"category_slug": "avtomobili"}
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Лимузин")
+        self.assertContains(response, "Tesla Cybertruck")
+        self.assertTemplateUsed(response, "goods/catalog.html")
+
+    def test_filter_on_sale(self):
+        response = self.client.get(
+            "/catalog/avtomobili/?on_sale=on/",
+            kwargs={"category_slug": "avtomobili", "on_sale": "on"},
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Лимузин")
+        self.assertNotContains(response, "Tesla Cybertruck")
+        self.assertTemplateUsed(response, "goods/catalog.html")
+
+    def test_filter_by_price(self):
+        response = self.client.get(
+            "/catalog/avtomobili/?order_by=default&cost_1k=on/",
+            kwargs={"category_slug": "avtomobili", "cost_1k": "true"},
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Лимузин")
+        self.assertNotContains(response, "Tesla Cybertruck")
+        self.assertTemplateUsed(response, "goods/catalog.html")
+
+    def test_pagination(self):
+        for i in range(10):
+            Products.objects.create(
+                name=f"Product {i+1}",
+                slug=f"product{i+1}",
+                price=100,
+                discount=10,
+                quantity=1,
+                category_id=3,
+                id=3+i,
+            )
+        response = self.client.get(
+            '/catalog/test-category/', kwargs={'category_slug': 'Test Category'}
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Product 1")
+        self.assertContains(response, "Product 6")
+        self.assertNotContains(response, "Product 7")
+        self.assertTemplateUsed(response, "goods/catalog.html")
+
+    def test_goods_amount(self):
+        response = self.client.get(
+            '/catalog/avtomobili/', kwargs={'category_slug': 'avtomobili'})
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "goods/catalog.html")
+        self.assertIn('amount', response.context)
+        self.assertEqual(response.context['amount'], 2)
+
+    def test_goods_ending(self):
+        Products.objects.create(
+            name="test product",
+            slug="test-product",
+            price=100,
+            discount=10,
+            quantity=1,
+            category_id=3,
+            id=3,
+        )
+        
+        response = self.client.get(
+            '/catalog/avtomobili/', kwargs={'category_slug': 'avtomobili'}
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "goods/catalog.html")
+        self.assertIn('goods_ending', response.context)
+        self.assertEqual(response.context['goods_ending'], 'товара')
+
+        response2 = self.client.get(
+            '/catalog/knigi/', kwargs={'category_slug': 'knigi'}
+        )
+        self.assertEqual(response2.context['goods_ending'], 'товаров')
+        
+        response3 = self.client.get(
+            '/catalog/test-category/', kwargs={'category_slug': 'Test Category'}
+        )
+        self.assertEqual(response3.context['goods_ending'], 'товар')
+        
+    def test_goods_name(self):
+        response = self.client.get(
+            '/catalog/avtomobili/', kwargs={'category_slug': 'avtomobili'})
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.context['goods'][0][0].name, 'Лимузин')
+        
+    def test_goods_rating(self):
+        response = self.client.get(
+            '/catalog/avtomobili/', kwargs={'category_slug': 'avtomobili'})
+        self.assertEqual(response.status_code, 200)    
+        self.assertEqual(response.context['goods'][0][1][0], 4)
+        
+    def test_goods_amount_reviews(self):
+        response = self.client.get(
+            '/catalog/avtomobili/', kwargs={'category_slug': 'avtomobili'})
+        self.assertEqual(response.status_code, 200)
+        
+        self.assertEqual(response.context['goods'][0][1][1], '1 отзыв')
+        self.assertEqual(response.context['goods'][0][1][2], 1)
+        
+        self.assertEqual(response.context['goods'][1][1][1], '0 отзывов')
+        self.assertEqual(response.context['goods'][1][1][2], 0)
+        
+    def test_create_review(self):
+        review = Review.objects.create(
+            product=self.product2,
+            user=self.test_user1,
+            rating=5,
+            comment='Отличный продукт',
+        )
+        self.assertIn(review, self.product2.reviews.all())
+        self.assertEqual(self.product2.reviews.count(), 1)
+        self.assertEqual(review.product, self.product2)
+        self.assertEqual(review.user, self.test_user1)
+        self.assertEqual(review.rating, 5)
+        self.assertEqual(review.comment, 'Отличный продукт')
+        self.assertIsNotNone(review.created)
--- core/apps/orders/tests.py
+++ core/apps/orders/tests.py
@@ -1,13 +1,21 @@
 from django.test import TestCase
+from django.urls import reverse
 
+from carts.models import Cart
+from orders.forms import CreateOrderForm
+from orders.models import Order, OrderItem
+from goods.models import Categories, Products
 from users.models import User
 
 
 class TestOrders(TestCase):
     
     def setUp(self):
-        test_user1 = User.objects.create_user(username='testuser1', password='12345')
-        test_user1.save()
+        Categories.objects.create(name='test_category', slug='test-category', id=1)     
+        self.product = Products.objects.create(
+            name='Test Product', image='1', price=100, discount=10, quantity=10, category_id=1, slug='test-product')
+        self.test_user1 = User.objects.create_user(username='testuser1', password='12345')
+        self.test_user1.save()
         
     def test_redirect_if_user_not_logged(self):
         
@@ -30,3 +38,73 @@ class TestOrders(TestCase):
         response2 = self.client.get('/orders/my-orders/')
         self.assertEqual(response2.status_code, 200)
         self.assertTemplateUsed(response2, 'orders/my_orders.html')
+        
+    def test_create_order(self):
+        login = self.client.login(username='testuser1', password='12345')
+        cart = Cart.objects.create(user=self.test_user1, product=self.product, quantity=1)
+        response = self.client.post(reverse('orders:create_order'), {
+            'first_name': 'Test',
+            'last_name': 'User',
+            'phone_number': '1234567890',
+            'requires_delivery': '1',
+            'delivery_address': 'Test Address',
+            'payment_on_get': '0',
+        })
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(Cart.objects.count(), 0)
+        self.assertTrue(Order.objects.filter(user=self.test_user1).exists())
+        self.assertEqual(Order.objects.count(), 1)
+        
+    def test_create_order_invalid_phone(self):
+        login = self.client.login(username='testuser1', password='12345')
+        cart = Cart.objects.create(user=self.test_user1, product=self.product, quantity=1)
+        
+        form_data = {
+            'first_name': 'Test',
+            'last_name': 'User',
+            'phone_number': 'invalid_phone',
+            'requires_delivery': '1',
+            'delivery_address': 'Test Address',
+            'payment_on_get': '0',
+        }
+        form = CreateOrderForm(data=form_data)
+        self.assertFalse(form.is_valid())
+        self.assertIn('phone_number', form.errors)
+        
+    def test_create_order_invalid_name(self):
+        login = self.client.login(username='testuser1', password='12345')
+        cart = Cart.objects.create(user=self.test_user1, product=self.product, quantity=1)
+        
+        response = self.client.post(reverse('orders:create_order'), {
+            'first_name': '',
+            'last_name': 'User',
+            'phone_number': '1234567890',
+            'requires_delivery': '1',
+            'delivery_address': 'Test Address',
+            'payment_on_get': '0',
+        })
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(Cart.objects.count(), 1)
+        self.assertEqual(Order.objects.count(), 0)
+    
+    def test_order_item_creation(self):
+        login = self.client.login(username='testuser1', password='12345')
+        cart = Cart.objects.create(user=self.test_user1, product=self.product, quantity=10)
+
+        order = Order.objects.create(
+            user=self.test_user1,
+            phone_number='1234567890',
+            requires_delivery=True,
+            delivery_address='Test Address',
+            payment_on_get=False,
+        )
+
+        OrderItem.objects.create(
+            order=order,
+            product=self.product,
+            name=self.product.name,
+            price=self.product.price,
+            quantity=10,
+        )
+
+        self.assertEqual(OrderItem.objects.count(), 1)
